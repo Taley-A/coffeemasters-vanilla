952dd5225650be2fa1603bb670c85a9dbbd833c8
Taley Ahmed <you@example.com>
2023-12-13 17:41:11 -0500

    dynamic routing

--- services/Router.js
+++ services/Router.js
@@ -7,6 +7,12 @@ const Router = {
 				Router.go(url);
 			});
 		});
+
+		// an event handler for URL changes
+		window.addEventListener("popstate", (event) => {
+			Router.go(event.state.route, false);
+		});
+
 		// check initial URL
 		Router.go(location.pathname);
 	},
@@ -26,8 +32,26 @@ const Router = {
 				pageElement = document.createElement("h1");
 				pageElement.textContent = "Your order";
 				break;
+			default:
+				if (route.startsWith("/products-")) {
+					pageElement = document.createElement("h1");
+					pageElement.textContent = "Details";
+					const paramId = route.substring(route.lastIndeOf("-") + 1);
+					pageElement.dataset.id = paramId;
+				}
 		}
-		document.querySelector("main").appendChild(pageElement);
+
+		if (pageElement) {
+			const cachedElement = document.querySelector("main");
+
+			// to remove previous injected elements
+			// document.querySelector("main").children[0].remove()
+
+			cachedElement.innerHTML = "";
+			cachedElement.appendChild(pageElement);
+			window.scrollX = 0;
+			window.scrollY = 0;
+		}
 	},
 };
 
